48a333f8047bc05c18137a03d8476e7ab2131711
muku <mukulbadhan@gmail.com>
2022-02-23 21:26:40 -0800

    column names correction

--- main.py
+++ main.py
@@ -7,20 +7,22 @@ if __name__ == '__main__':
         .appName("airport") \
         .getOrCreate()
         # .config("spark.driver.memory", "15g") \
+    1, "Goroka Airport", "Goroka", "Papua New Guinea", "GKA", "AYGA", -6.081689834590001, 145.391998291, 5282, 10, "U", "Pacific/Port_Moresby", "airport", "OurAirports"
 
     SCHEMA = StructType([StructField('Airport id', IntegerType()),
                          StructField('Name', StringType()),
                          StructField('city', StringType()),
                          StructField('Country', StringType()),
-                         StructField('Code', StringType()),
-                         StructField('Code2', StringType()),
-                         StructField('Decimal Degree', DoubleType()),
-                         StructField('Hours offset', DoubleType()),
-                         StructField('dk1', IntegerType()),
-                         StructField('dk2', IntegerType()),
-                         StructField('Timezone', StringType()),
-                         StructField('airport', StringType()),
-                         StructField('airport2', StringType())])
+                         StructField('IATA Code', StringType()),
+                         StructField('ICAO Code', StringType()),
+                         StructField('Latitude', DoubleType()),
+                         StructField('Longitude', DoubleType()),
+                         StructField('Altitude', IntegerType()),
+                         StructField('Timezone', IntegerType()),
+                         StructField('DST', StringType()),
+                         StructField('Tz', StringType()),
+                         StructField('Type', StringType()),
+                         StructField('Source', StringType())])
     # Prepare training and test data.
     data = spark.read.schema(SCHEMA).option("header", False).option("delimiter", ",") \
         .csv("airports.dat")
