f00386b31b2b9f5888f97d5fdd62399d28848ea0
muku <mukulbadhan@gmail.com>
2022-02-24 19:03:24 -0800

    joins and dataframe function

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, DoubleType, IntegerType, StringType
 
-if __name__ == '__main__':
-
-    spark = SparkSession.builder.master("local[1]") \
-        .appName("airport") \
-        .getOrCreate()
-        # .config("spark.driver.memory", "15g") \
-
+def get_airports(spark):
     SCHEMA = StructType([StructField('id', IntegerType()),
                          StructField('Name', StringType()),
                          StructField('city', StringType()),
@@ -25,5 +19,30 @@ if __name__ == '__main__':
     # Prepare training and test data.
     data = spark.read.schema(SCHEMA).option("header", False).option("delimiter", ",") \
         .csv("airports.dat")
+    return data
+def get_airlines(spark):
+    SCHEMA = StructType([StructField('id', IntegerType()),
+                         StructField('Name', StringType()),
+                         StructField('Alias', StringType()),
+                         StructField('IATA', StringType()),
+                         StructField('ICAO', StringType()),
+                         StructField('Callsign', StringType()),
+                         StructField('Country', StringType()),
+                         StructField('Active', StringType())])
+    # Prepare training and test data.
+    data = spark.read.schema(SCHEMA).option("header", False).option("delimiter", ",") \
+        .csv("airlines.dat")
+    return data
+if __name__ == '__main__':
+
+    spark = SparkSession.builder.master("local[1]") \
+        .appName("airport") \
+        .getOrCreate()
+        # .config("spark.driver.memory", "15g") \
+
+    data= get_airports(spark)
+    airlines = get_airlines(spark)
+    activeUS= airlines.where("Country='United States' and Active='Y'")
     #
-    data.where("id=2").show(10)
+    usAirport=data.where("Country='United States'")
+    usAirport.join(activeUS,"Country").show(1)
