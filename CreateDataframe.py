6e7b8a70e2868adb2b6daba1d121677fc1468fef
muku <mukulbadhan@gmail.com>
2022-03-01 18:37:13 -0800

    abstraction

---
+++ CreateDataframe.py
@@ -0,0 +1,36 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, DoubleType, IntegerType, StringType
+
+def get_airports(spark):
+    SCHEMA = StructType([StructField('id', IntegerType()),
+                         StructField('Name', StringType()),
+                         StructField('city', StringType()),
+                         StructField('Country', StringType()),
+                         StructField('IATA Code', StringType()),
+                         StructField('ICAO Code', StringType()),
+                         StructField('Latitude', DoubleType()),
+                         StructField('Longitude', DoubleType()),
+                         StructField('Altitude', IntegerType()),
+                         StructField('Timezone', IntegerType()),
+                         StructField('DST', StringType()),
+                         StructField('Tz', StringType()),
+                         StructField('Type', StringType()),
+                         StructField('Source', StringType())])
+    # Prepare training and test data.
+    data = spark.read.schema(SCHEMA).option("header", False).option("delimiter", ",") \
+        .csv("airports.dat")
+    return data
+
+def get_airlines(spark):
+    SCHEMA = StructType([StructField('id', IntegerType()),
+                         StructField('Name', StringType()),
+                         StructField('Alias', StringType()),
+                         StructField('IATA', StringType()),
+                         StructField('ICAO', StringType()),
+                         StructField('Callsign', StringType()),
+                         StructField('Country', StringType()),
+                         StructField('Active', StringType())])
+    # Prepare training and test data.
+    data = spark.read.schema(SCHEMA).option("header", False).option("delimiter", ",") \
+        .csv("airlines.dat")
+    return data
--- main.py
+++ main.py
@@ -1,48 +1,34 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, DoubleType, IntegerType, StringType
 
-def get_airports(spark):
-    SCHEMA = StructType([StructField('id', IntegerType()),
-                         StructField('Name', StringType()),
-                         StructField('city', StringType()),
-                         StructField('Country', StringType()),
-                         StructField('IATA Code', StringType()),
-                         StructField('ICAO Code', StringType()),
-                         StructField('Latitude', DoubleType()),
-                         StructField('Longitude', DoubleType()),
-                         StructField('Altitude', IntegerType()),
-                         StructField('Timezone', IntegerType()),
-                         StructField('DST', StringType()),
-                         StructField('Tz', StringType()),
-                         StructField('Type', StringType()),
-                         StructField('Source', StringType())])
-    # Prepare training and test data.
-    data = spark.read.schema(SCHEMA).option("header", False).option("delimiter", ",") \
-        .csv("airports.dat")
-    return data
-def get_airlines(spark):
-    SCHEMA = StructType([StructField('id', IntegerType()),
-                         StructField('Name', StringType()),
-                         StructField('Alias', StringType()),
-                         StructField('IATA', StringType()),
-                         StructField('ICAO', StringType()),
-                         StructField('Callsign', StringType()),
-                         StructField('Country', StringType()),
-                         StructField('Active', StringType())])
-    # Prepare training and test data.
-    data = spark.read.schema(SCHEMA).option("header", False).option("delimiter", ",") \
-        .csv("airlines.dat")
-    return data
-if __name__ == '__main__':
+from CreateDataframe import get_airports, get_airlines
 
+
+def active_us_airline(spark):
+    data = get_airports(spark)
+    print("45")
+    # airlines = get_airlines(spark)
+    # activeUS = airlines.where("Country='United States' and Active='Y'")
+    # usAirport = data.where("Country='United States'")
+    # usAirport.join(activeUS, "Country").show(1)
+
+
+def passf():
+    print("koi")
+    pass
+
+
+def problem(i):
     spark = SparkSession.builder.master("local[1]") \
-        .appName("airport") \
+        .appName("airport6") \
         .getOrCreate()
-        # .config("spark.driver.memory", "15g") \
+    switcher = {
+        0: active_us_airline(spark),
+        1: passf
+    }
+    func = switcher.get(i, lambda: 'Invalid')
+    return func()
 
-    data= get_airports(spark)
-    airlines = get_airlines(spark)
-    activeUS= airlines.where("Country='United States' and Active='Y'")
-    #
-    usAirport=data.where("Country='United States'")
-    usAirport.join(activeUS,"Country").show(1)
+
+if __name__ == '__main__':
+    prob = 1
+    problem(prob)
